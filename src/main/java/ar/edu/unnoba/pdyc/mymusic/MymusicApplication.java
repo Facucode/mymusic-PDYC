ea7c1590e5cc6fa90d2076d51162073bb8149032
Facucode <34733879+Facucode@users.noreply.github.com>
2021-04-29 15:51:15 -0300

    Project initial commit

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/MymusicApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.mymusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unnoba/pdyc/mymusic/MymusicApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.mymusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
