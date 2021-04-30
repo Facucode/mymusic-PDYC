ba0e64a7ad8b1dfd0f712cebc2ee7df98d5f4551
Facucode <34733879+Facucode@users.noreply.github.com>
2021-04-30 14:54:37 -0300

    Little changes

--- src/main/java/ar/edu/unnoba/pdyc/mymusic/JerseyConfig.java
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/JerseyConfig.java
@@ -1,9 +1,10 @@
 package ar.edu.unnoba.pdyc.mymusic;
 
+import ar.edu.unnoba.pdyc.mymusic.resource.SongResource;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
-    public JerseyConfig(){}
+    public JerseyConfig(){register(SongResource.class);}
 }
