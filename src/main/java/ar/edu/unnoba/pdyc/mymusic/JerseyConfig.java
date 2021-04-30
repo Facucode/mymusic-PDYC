1b3f56dc743b201404f71b65e97471224d6d646a
Facucode <34733879+Facucode@users.noreply.github.com>
2021-04-30 11:13:13 -0300

    Jersey configuration

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/JerseyConfig.java
@@ -0,0 +1,9 @@
+package ar.edu.unnoba.pdyc.mymusic;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig(){}
+}
